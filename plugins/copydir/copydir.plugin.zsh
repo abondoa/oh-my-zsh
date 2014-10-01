0ce9aab480f2ae37a4b344c9678bf7080c97f087
alex <abondoa@gmail.com>
2014-10-01 21:41:54 +0200

    copydir and pbcopy added

--- plugins/copydir/copydir.plugin.zsh
+++ plugins/copydir/copydir.plugin.zsh
@@ -1,3 +1,14 @@
 function copydir {
-  pwd | tr -d "\r\n" | pbcopy
+    if [[ "$#" == 1 ]] then
+        if [[ -a $1 ]] then
+            pushd $1 &> /dev/null
+            pwd | tr -d "\r\n" | pbcopy
+            popd &> /dev/null
+        else
+            echo "copydir: no such file: $1" > /dev/stderr
+            return 1
+        fi
+    else
+        pwd | tr -d "\r\n" | pbcopy
+    fi
 }
