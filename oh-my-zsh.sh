c4d5fd878a273d178e21d5af84fe726a88395787
alex <abondoa@gmail.com>
2014-10-21 22:00:12 +0200

    change plugin created

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -20,7 +20,7 @@ if [[ -z "$ZSH_CUSTOM" ]]; then
     ZSH_CUSTOM="$ZSH/custom"
 fi
 
-plugins=($plugins pbcopy command-not-found mvn mercurial pip python screen sublime svn web-search autojump svn-fast-info git cp copydir copyfile)
+plugins=($plugins change pbcopy command-not-found mvn mercurial pip python screen sublime svn web-search autojump svn-fast-info git cp copydir copyfile)
 typeset -U plugins
 
 is_plugin() {
